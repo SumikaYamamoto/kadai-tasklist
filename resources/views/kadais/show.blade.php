d11cc97874789d638ec43a60e2729d909dad0015
jiro.kirameki <ec2-user@ip-172-31-29-130.us-east-2.compute.internal>
2018-05-29 06:17:14 +0000

    CRUD kadais

---
+++ resources/views/kadais/show.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+
+ <h1>id = {{ $kadai->id }} のタスク詳細ページ</h1>
+
+    <p>{{ $kadai->content }}</p>
+    
+    {!! link_to_route('kadais.edit', 'このタスクを編集', ['id' => $kadai->id]) !!}
+    
+    {!! Form::model($kadai, ['route' => ['kadais.destroy', $kadai->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除') !!}
+    {!! Form::close() !!}
+
+@endsection
